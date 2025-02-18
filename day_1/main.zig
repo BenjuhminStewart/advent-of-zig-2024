3d4d1f8296c2fa716cb337db531347894efc5682
Benjamin Stewart <82689821+BenjuhminStewart@users.noreply.github.com>
2025-02-17 17:40:36 -0800

    refactor: day 1 refactor (#18)

--- day_1/main.zig
+++ day_1/main.zig
@@ -5,78 +5,98 @@ const Allocator = std.mem.Allocator;
 const parseInt = std.fmt.parseInt;
 
 const data = @embedFile("input.txt");
+var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
+const gpa = gpa_impl.allocator();
+
+var alloc: std.mem.Allocator = undefined;
 
 pub fn main() !void {
-    const score = getSimilarityScore(data);
-    const distance = getDistance(data);
-
-    print("Distance: {d}\n", .{distance});
-    print("Similarity Score: {d}\n", .{score});
+    try part1();
+    try part2();
 }
 
-pub fn getDistance(input: []const u8) u32 {
-    var distance: u32 = 0;
-    var left: [1000]i32 = undefined;
-    var right: [1000]i32 = undefined;
+pub fn part1() !void {
+    var arena = std.heap.ArenaAllocator.init(gpa);
+    defer arena.deinit();
+    alloc = arena.allocator();
 
-    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    left = std.ArrayList(i32).init(alloc);
+    right = std.ArrayList(i32).init(alloc);
+
+    try parse();
+
+    const part_1 = getDistance();
+    print("part 1: {d}\n", .{part_1});
+}
+
+pub fn part2() !void {
+    var arena = std.heap.ArenaAllocator.init(gpa);
+    defer arena.deinit();
+    alloc = arena.allocator();
+
+    left = std.ArrayList(i32).init(alloc);
+    right = std.ArrayList(i32).init(alloc);
+
+    try parse();
+
+    const part_2 = getSimilarityScore();
+    print("part 2: {d}\n", .{part_2});
+}
+
+var left: std.ArrayList(i32) = undefined;
+var right: std.ArrayList(i32) = undefined;
+
+pub fn parse() !void {
+    var lines = std.mem.tokenizeScalar(u8, data, '\n');
     var i: usize = 0;
     while (lines.next()) |token| {
         var line = std.mem.tokenizeScalar(u8, token, ' ');
         while (line.next()) |number| {
             if (i % 2 == 0) {
-                left[i / 2] = parseInt(i32, number, 10) catch unreachable;
+                const num = parseInt(i32, number, 10) catch {
+                    print("error parsing number: {s}\n", .{number});
+                    return error.ParseError;
+                };
+                left.append(num) catch return {
+                    return error.OutOfMemory;
+                };
             } else {
-                right[i / 2] = parseInt(i32, number, 10) catch unreachable;
+                const num = parseInt(i32, number, 10) catch {
+                    print("error parsing number: {s}\n", .{number});
+                    return error.ParseError;
+                };
+                right.append(num) catch return {
+                    return error.OutOfMemory;
+                };
             }
             i += 1;
         }
     }
+}
 
-    std.mem.sort(i32, &left, {}, std.sort.asc(i32));
-    std.mem.sort(i32, &right, {}, std.sort.asc(i32));
+pub fn getDistance() u32 {
+    var distance: u32 = 0;
+    std.mem.sort(i32, left.items, {}, std.sort.asc(i32));
+    std.mem.sort(i32, right.items, {}, std.sort.asc(i32));
 
-    for (left, right) |left_word, right_word| {
+    for (left.items, right.items) |left_word, right_word| {
         const local_distance = right_word - left_word;
         distance += @abs(local_distance);
     }
 
     return distance;
 }
-
-pub fn getSimilarityScore(input: []const u8) i32 {
+pub fn getSimilarityScore() i32 {
     var score: i32 = 0;
 
-    var left: [1000]i32 = undefined;
-    var right: [1000]i32 = undefined;
-
-    var lines = std.mem.tokenizeScalar(u8, input, '\n');
-    var i: usize = 0;
-    while (lines.next()) |token| {
-        var line = std.mem.tokenizeScalar(u8, token, ' ');
-        while (line.next()) |number| {
-            if (i % 2 == 0) {
-                left[i / 2] = parseInt(i32, number, 10) catch unreachable;
-            } else {
-                right[i / 2] = parseInt(i32, number, 10) catch unreachable;
-            }
-            i += 1;
-        }
-    }
-
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-
-    const allocator = gpa.allocator();
-
-    var map = std.AutoHashMap(i32, i32).init(allocator);
+    var map = std.AutoHashMap(i32, i32).init(alloc);
     defer map.deinit();
 
-    for (left) |left_word| {
+    for (left.items) |left_word| {
         map.put(left_word, 0) catch unreachable;
     }
 
-    for (right) |right_word| {
+    for (right.items) |right_word| {
         if (map.get(right_word)) |count| {
             map.put(right_word, count + 1) catch unreachable;
         } else {
@@ -84,7 +104,7 @@ pub fn getSimilarityScore(input: []const u8) i32 {
         }
     }
 
-    for (left) |left_word| {
+    for (left.items) |left_word| {
         score += left_word * map.get(left_word).?;
     }
     return score;
