1b09b27c11316e66e6eb0cdd9872d9fe434338a3
Benjamin Stewart <benjuhminstewart@gmail.com>
2025-02-21 20:54:12 -0800

    refactor: update run_all.sh - easier to switch zig version and run all

--- run_all.sh
+++ run_all.sh
@@ -1,26 +1,29 @@
 #!/bin/zsh
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_1/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_2/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_3/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_4/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_5/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_6/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_7/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_8/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_9/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_10/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_11/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_12/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_13/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_14/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_15/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_16/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_17/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_18/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_19/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_20/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_21/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_22/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_23/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_24/main.zig
-~/zig/0.14.0-dev.3239+d7b93c787/files/zig run day_25/main.zig
+
+ZIG_PATH=~/zig/0.14.0-dev.3280+bbd13ab96/files/zig
+
+$ZIG_PATH run day_1/main.zig
+$ZIG_PATH run day_2/main.zig
+$ZIG_PATH run day_3/main.zig
+$ZIG_PATH run day_4/main.zig
+$ZIG_PATH run day_5/main.zig
+$ZIG_PATH run day_6/main.zig
+$ZIG_PATH run day_7/main.zig
+$ZIG_PATH run day_8/main.zig
+$ZIG_PATH run day_9/main.zig
+$ZIG_PATH run day_10/main.zig
+$ZIG_PATH run day_11/main.zig
+$ZIG_PATH run day_12/main.zig
+$ZIG_PATH run day_13/main.zig
+$ZIG_PATH run day_14/main.zig
+$ZIG_PATH run day_15/main.zig
+$ZIG_PATH run day_16/main.zig
+$ZIG_PATH run day_17/main.zig
+$ZIG_PATH run day_18/main.zig
+$ZIG_PATH run day_19/main.zig
+$ZIG_PATH run day_20/main.zig
+$ZIG_PATH run day_21/main.zig
+$ZIG_PATH run day_22/main.zig
+$ZIG_PATH run day_23/main.zig
+$ZIG_PATH run day_24/main.zig
+$ZIG_PATH run day_25/main.zig
